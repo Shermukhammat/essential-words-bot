961ba6cb164fddf797ac3ffe3fae1812c4b75fef
SHermukhammad <shermukhammadtemirov@gmail.com>
2024-10-29 09:17:00 -0700

    extracted pre data from anki drodi flashcard files

--- utilites/buttons/inline.py
+++ utilites/buttons/inline.py
@@ -7,6 +7,6 @@ class InlineButtons:
     def wordlist_buttons(book : int, unit  : int):
         return InlineKeyboardMarkup(
             inline_keyboard=[
-                [InlineKeyboardButton(text="🖼 Rasim", callback_data=f"photo={book}&unit={unit}"), InlineKeyboardButton(text="🎧 Audio", switch_inline_query_current_chat=f"book{book}.unit{unit}")]
+                [InlineKeyboardButton(text="🖼 Rasim", callback_data=f"photo={book}&unit={unit}"), InlineKeyboardButton(text="🎧 Audio", switch_inline_query_current_chat=f"{book}&{unit}")]
             ]
         )
